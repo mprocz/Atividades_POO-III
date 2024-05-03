b313ac245b5ab5beb451fb9906a18eae846828c7
mprocz <59449440+mprocz@users.noreply.github.com>
2024-05-03 09:35:20 -0300

    Delete atividade03/src/delivery directory

--- atividade03/src/delivery/Company.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    IFOOD: "IFOOD",
-    AIQFOME: "AIQFOME"
-}
--- atividade03/src/delivery/interfaces/IDeliveryFactory.ts
+++
@@ -1,7 +0,0 @@
-import IDrink from "../../foods/drink/interfaces/IDrink";
-import IFood from "../../foods/food/interfaces/IFood";
-
-export default interface IDeliveryFactory {
-    createDeliveryDrink(): IDrink;
-    creatDeliveryFood(): IFood;
-}
